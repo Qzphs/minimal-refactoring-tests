8471cfd9f73d5c5a01020bc8880145120f85f575
Qzphs <86579079+Qzphs@users.noreply.github.com>
2023-05-24 12:36:37 +1200

    Add some printing methods

--- src/Base.java
+++ src/Base.java
@@ -1,4 +1,9 @@
 public class Base {
     protected int x;
     public Base() { this.x = 0; }
+    public void printX() { System.out.println(this.x); }
+    public void printXTwice() {
+        printX();
+        printX();
+    }
 }

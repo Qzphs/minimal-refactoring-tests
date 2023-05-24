6ac4e3be566399bfdf3e8d258c83da800b6314f9
Qzphs <86579079+Qzphs@users.noreply.github.com>
2023-05-24 12:13:26 +1200

    Pull down field x

--- src/Base.java
+++ src/Base.java
@@ -1,5 +1,3 @@
 public class Base {
-    private int x;
     public Base() {}
-    public Base(int x) { this.x = x; }
 }
--- src/Derived.java
+++ src/Derived.java
@@ -1,4 +1,8 @@
 public class Derived extends Base {
+    private int x;
     public Derived() { super(); }
-    public Derived(int x) { super(x); }
+    public Derived(int x) {
+        super();
+        this.x = x;
+    }
 }

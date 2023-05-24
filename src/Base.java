3a6e36250d7509e681b58b386a83fc06cc48e9d6
Qzphs <86579079+Qzphs@users.noreply.github.com>
2023-05-24 12:36:55 +1200

    Rename printXTwice to printXTwoTimes

--- src/Base.java
+++ src/Base.java
@@ -2,7 +2,7 @@ public class Base {
     protected int x;
     public Base() { this.x = 0; }
     public void printX() { System.out.println(this.x); }
-    public void printXTwice() {
+    public void printXTwoTimes() {
         printX();
         printX();
     }

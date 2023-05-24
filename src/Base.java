6db6deb717de1cb69703a17eb5cb70fad38ed3bf
Qzphs <86579079+Qzphs@users.noreply.github.com>
2023-05-24 12:52:09 +1200

    Remove printXTwoTimes

--- src/Base.java
+++ src/Base.java
@@ -2,8 +2,4 @@ public class Base {
     protected int x;
     public Base() { this.x = 0; }
     public void printX() { System.out.println(this.x); }
-    public void printXTwoTimes() {
-        printX();
-        printX();
-    }
 }
